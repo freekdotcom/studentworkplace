e4ecebf06dfa8fc723b7a61b4c97d83f741e98f2
Guus van Gend <guus.van.gend@hva.nl>
2016-12-22 14:36:59 +0100

    Logout plus add items

---
+++ app/src/main/java/com/example/frederick/studentworkplace/Item.java
@@ -0,0 +1,24 @@
+package com.example.frederick.studentworkplace;
+
+/**
+ * Created by Guus on 22-12-2016.
+ */
+
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- app/src/main/java/com/example/frederick/studentworkplace/MainActivity.java
+++ app/src/main/java/com/example/frederick/studentworkplace/MainActivity.java
@@ -9,13 +9,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +39,58 @@ public class MainActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         if (mFirebaseUser == null) {
             // Not logged in, launch the Log In activity
             loadLogInView();
         }
+        else {
+            mUserId = mFirebaseUser.getUid();
 
+            final ListView listView = (ListView) findViewById(R.id.listView);
+            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+            listView.setAdapter(adapter);
 
+            //Add items via the Button and EditText at the bottom of the view.
+            final EditText text = (EditText)findViewById(R.id.todoText);
+            final Button button = (Button) findViewById(R.id.addButton);
+            button.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Item item = new Item(text.getText().toString());
+                    mDatabase.child("users").child(mUserId).child("items").push().setValue(item);
+                    text.setText("");
+                }
+            });
 
+            // Use Firebase to populate the List.
+            mDatabase.child("users").child(mUserId).child("items").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    adapter.add((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    adapter.remove((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 
     @Override
@@ -61,7 +117,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
-            return true;
+            mFirebaseAuth.signOut();
+            loadLogInView();
         }
 
         return super.onOptionsItemSelected(item);
